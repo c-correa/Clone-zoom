c831de3deaa31ed1aa75d2dbaa31cc81a9a76b80
c-correa <19correa99@gmail.com>
2024-11-05 23:14:48 -0500

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import productRoutes from './products/product.routes';
-
+import cors from 'cors';
 const app = express();
+
+app.use(cors({
+    origin: 'https://technical-test-api-node.onrender.com', // Cambia con tu dominio
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'] // Encabezados permitidos
+  }));
 
 app.use(express.json());
 app.use('/products', productRoutes);
